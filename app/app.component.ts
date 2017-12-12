6b3e3471f96ced54d37ff385d1aedb26668f8018
Igor Ivanov <igorpi87@yandex.ru>
2017-12-12 17:28:52 +0300

    first

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+// const todos = ["Изучить ангуляр", "fbdhfbhdbf", "dfdfdfdfdf"];
+
+@Component({
+  moduleId: module.id,
+  selector: "app",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.css"]
+})
+export class AppComponent {
+  title: "Anhhhar";
+  todos: ["Изучить ангуляр", "fbdhfbhdbf", "dfdfdfdfdf"];
+}
